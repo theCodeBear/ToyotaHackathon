67682e1edb4faeb747ea38fb1a5ab0a7c0976df3
teleaziz <abdz.abbas@gmail.com>
2014-11-08 11:25:01 -0800

    add route for cars/id

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var http = require('http);
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/cars/:id', function(req,res){
+res.json("Ok");
+
+})
 
 module.exports = router;
