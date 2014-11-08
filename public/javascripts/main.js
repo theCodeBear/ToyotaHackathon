245007680d08d62c540f2501366534f44a3fec15
Ryan Janvier <ryan@liquidtrends.com>
2014-11-08 04:44:10 -0800

    Add ui, css, js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,13 @@
 $(function() {
 
   // var request = require('request');
-
-  var params = {developerkey: bea1d5e2e441, responseformat: json, vid: ITCUS_VID_052, infoids: [Pson]};
     $.ajax({
         type: "POST",
-        data: JSON.stringify(params),
         url: "https://api-jp-t-itc.com/GetVehicleInfo",
-        contentType: "application/json",
-        success: function(data) { console.log(data) }
+        data: {'developerkey':'bea1d5e2e441','responseformat':'json','vid':'ITCUS_VID_052','infoids': '[Pson]'},
+        contentType: "jsonp",
+        crossDomain: true,
+        success: function(data) { console.log(data) },
     });
-
-
 
 });
